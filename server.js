8f1046a1740c6bb7367a1027a6e829b37eb6b9e2
Hammourii17 <a-7moory@hotmail.com>
2024-09-21 19:54:33 +0300

    made some changes for the db connection , and also for verifying the jwt

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const subjectRoutes = require('./routes/subjectRoutes');
+const cors = require('cors');
+
+
 
 dotenv.config();
 const app = express();
@@ -11,14 +14,19 @@ const app = express();
 app.use(express.json());
 
 connectDB();
-
+app.use(cors({ origin: '*' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/subjects', subjectRoutes);  
 
+
 const PORT = process.env.PORT || 5003;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server running on port ${PORT}`);
     });
-
+    connectDB().then(() => {
+        console.log('MongoDB Connected');
+    }).catch((err) => {
+        console.error('MongoDB connection failed:', err.message);
+    });
